c62bde1d77e88a1aeb93069d9a8d9245d3ee3750
Wallace3101 <williamasaravialeon111003@gmail.com>
2024-11-17 18:27:45 -0500

    Creacion del APIview para poder registrar usuarios

--- Nexify/Nexify/urls.py
+++ Nexify/Nexify/urls.py
@@ -7,7 +7,8 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('NexifyApp.urls')),
-    path('login', views.LoginView.as_view(), name='login'),
+    path('login/', views.LoginView.as_view(), name='login'),
+    path('register/', views.RegisterView.as_view(), name='register'),
 ]
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
